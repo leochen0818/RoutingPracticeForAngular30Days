9bc7928ce107794ba0b595b92b5e79dcda5a047d
Leo Chen <a0952250818@gmail.com>
2018-11-04 20:11:30 +0800

    完成巢狀路由的設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,31 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ]
   },
   {
     path: 'login',
     component: LoginComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
   },
   {
     path: '**',
