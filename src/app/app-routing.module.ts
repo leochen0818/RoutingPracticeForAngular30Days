146d0599e6c23bd0ba23c7041bfbc16c9c2cefcf
Leo Chen <a0952250818@gmail.com>
2018-11-07 21:27:30 +0800

    完成預先載入之設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 // Component
 import { AboutComponent } from './about/about.component';
@@ -44,7 +44,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
